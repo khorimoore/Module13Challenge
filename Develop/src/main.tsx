148d0fc216576e311490991ff5f3e0a1ec7ac7b4
khori moore <khorimoore23@gmail.com>
2024-10-31 11:33:40 -0700

    staged main.tsx page

--- Develop/src/main.tsx
+++ Develop/src/main.tsx
@@ -3,7 +3,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 
 import App from './App.tsx';
-import CandidateSearch from './pages/CandidateSearch.tsx';
+import CandidateSearch from './pages/CandidateSearch.tsx'; 
 import SavedCandidates from './pages/SavedCandidates.tsx';
 import ErrorPage from './pages/ErrorPage.tsx';
 
